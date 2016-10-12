ba279e73eb063d4de45487b4ea3b35931f6cbcc7
comeonmissli <1312467852@qq.com>
2016-10-12 16:30:30 +0800

    Print"TextViewTest01"

---
+++ src/iet/jxufe/cn/android/MainActivity.java
@@ -0,0 +1,28 @@
+package iet.jxufe.cn.android;
+
+import com.example.textviewtest.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.Html;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		TextView html=(TextView)findViewById(R.id.html);
+		html.setText(Html.fromHtml("欢迎参加<font color=red>"+"手机软件设计赛</font>"));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
